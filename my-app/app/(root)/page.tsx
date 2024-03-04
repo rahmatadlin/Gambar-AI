cc2fcfd7132aeae471eb1e9f7f0c13af46292dda
rahmatadlin <rahmatadlin210@gmail.com>
2024-03-04 16:29:46 +0700

    sidebar

--- my-app/app/(root)/page.tsx
+++ my-app/app/(root)/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <p>Home</p>
 
-export default Home
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
+
+export default Home;
