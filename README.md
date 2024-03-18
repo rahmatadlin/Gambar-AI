7d37955fd2d0ac6723a63b5eb51bef13659ba926
rahmatadlin <rahmatadlin210@gmail.com>
2024-03-18 13:57:29 +0700

    upload

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ npx shadcn-ui@latest add sheet button
 npm install mongodb mongoose
 npm i qs
 npm i svix
+npx shadcn-ui@latest add form
+npx shadcn-ui@latest add input
+npx shadcn-ui@latest add select
--- lib/utils.ts
+++ lib/utils.ts
@@ -50,108 +50,109 @@ const toBase64 = (str: string) =>
 export const dataUrl = `data:image/svg+xml;base64,${toBase64(
   shimmer(1000, 1000)
 )}`;
+
 // ==== End
 
-// // FORM URL QUERY
-// export const formUrlQuery = ({
-//   searchParams,
-//   key,
-//   value,
-// }: FormUrlQueryParams) => {
-//   const params = { ...qs.parse(searchParams.toString()), [key]: value };
+// FORM URL QUERY
+export const formUrlQuery = ({
+  searchParams,
+  key,
+  value,
+}: FormUrlQueryParams) => {
+  const params = { ...qs.parse(searchParams.toString()), [key]: value };
 
-//   return `${window.location.pathname}?${qs.stringify(params, {
-//     skipNulls: true,
-//   })}`;
-// };
+  return `${window.location.pathname}?${qs.stringify(params, {
+    skipNulls: true,
+  })}`;
+};
 
-// // REMOVE KEY FROM QUERY
-// export function removeKeysFromQuery({
-//   searchParams,
-//   keysToRemove,
-// }: RemoveUrlQueryParams) {
-//   const currentUrl = qs.parse(searchParams);
+// REMOVE KEY FROM QUERY
+export function removeKeysFromQuery({
+  searchParams,
+  keysToRemove,
+}: RemoveUrlQueryParams) {
+  const currentUrl = qs.parse(searchParams);
 
-//   keysToRemove.forEach((key) => {
-//     delete currentUrl[key];
-//   });
+  keysToRemove.forEach((key) => {
+    delete currentUrl[key];
+  });
 
-//   // Remove null or undefined values
-//   Object.keys(currentUrl).forEach(
-//     (key) => currentUrl[key] == null && delete currentUrl[key]
-//   );
+  // Remove null or undefined values
+  Object.keys(currentUrl).forEach(
+    (key) => currentUrl[key] == null && delete currentUrl[key]
+  );
 
-//   return `${window.location.pathname}?${qs.stringify(currentUrl)}`;
-// }
+  return `${window.location.pathname}?${qs.stringify(currentUrl)}`;
+}
 
-// // DEBOUNCE
-// export const debounce = (func: (...args: any[]) => void, delay: number) => {
-//   let timeoutId: NodeJS.Timeout | null;
-//   return (...args: any[]) => {
-//     if (timeoutId) clearTimeout(timeoutId);
-//     timeoutId = setTimeout(() => func.apply(null, args), delay);
-//   };
-// };
+// DEBOUNCE
+export const debounce = (func: (...args: any[]) => void, delay: number) => {
+  let timeoutId: NodeJS.Timeout | null;
+  return (...args: any[]) => {
+    if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func.apply(null, args), delay);
+  };
+};
 
-// // GE IMAGE SIZE
-// export type AspectRatioKey = keyof typeof aspectRatioOptions;
-// export const getImageSize = (
-//   type: string,
-//   image: any,
-//   dimension: "width" | "height"
-// ): number => {
-//   if (type === "fill") {
-//     return (
-//       aspectRatioOptions[image.aspectRatio as AspectRatioKey]?.[dimension] ||
-//       1000
-//     );
-//   }
-//   return image?.[dimension] || 1000;
-// };
+// GE IMAGE SIZE
+export type AspectRatioKey = keyof typeof aspectRatioOptions;
+export const getImageSize = (
+  type: string,
+  image: any,
+  dimension: "width" | "height"
+): number => {
+  if (type === "fill") {
+    return (
+      aspectRatioOptions[image.aspectRatio as AspectRatioKey]?.[dimension] ||
+      1000
+    );
+  }
+  return image?.[dimension] || 1000;
+};
 
-// // DOWNLOAD IMAGE
-// export const download = (url: string, filename: string) => {
-//   if (!url) {
-//     throw new Error("Resource URL not provided! You need to provide one");
-//   }
+// DOWNLOAD IMAGE
+export const download = (url: string, filename: string) => {
+  if (!url) {
+    throw new Error("Resource URL not provided! You need to provide one");
+  }
 
-//   fetch(url)
-//     .then((response) => response.blob())
-//     .then((blob) => {
-//       const blobURL = URL.createObjectURL(blob);
-//       const a = document.createElement("a");
-//       a.href = blobURL;
+  fetch(url)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const blobURL = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = blobURL;
 
-//       if (filename && filename.length)
-//         a.download = `${filename.replace(" ", "_")}.png`;
-//       document.body.appendChild(a);
-//       a.click();
-//     })
-//     .catch((error) => console.log({ error }));
-// };
+      if (filename && filename.length)
+        a.download = `${filename.replace(" ", "_")}.png`;
+      document.body.appendChild(a);
+      a.click();
+    })
+    .catch((error) => console.log({ error }));
+};
 
-// // DEEP MERGE OBJECTS
-// export const deepMergeObjects = (obj1: any, obj2: any) => {
-//   if(obj2 === null || obj2 === undefined) {
-//     return obj1;
-//   }
+// DEEP MERGE OBJECTS
+export const deepMergeObjects = (obj1: any, obj2: any) => {
+  if(obj2 === null || obj2 === undefined) {
+    return obj1;
+  }
 
-//   let output = { ...obj2 };
+  let output = { ...obj2 };
 
-//   for (let key in obj1) {
-//     if (obj1.hasOwnProperty(key)) {
-//       if (
-//         obj1[key] &&
-//         typeof obj1[key] === "object" &&
-//         obj2[key] &&
-//         typeof obj2[key] === "object"
-//       ) {
-//         output[key] = deepMergeObjects(obj1[key], obj2[key]);
-//       } else {
-//         output[key] = obj1[key];
-//       }
-//     }
-//   }
+  for (let key in obj1) {
+    if (obj1.hasOwnProperty(key)) {
+      if (
+        obj1[key] &&
+        typeof obj1[key] === "object" &&
+        obj2[key] &&
+        typeof obj2[key] === "object"
+      ) {
+        output[key] = deepMergeObjects(obj1[key], obj2[key]);
+      } else {
+        output[key] = obj1[key];
+      }
+    }
+  }
 
-//   return output;
-// };
+  return output;
+};
